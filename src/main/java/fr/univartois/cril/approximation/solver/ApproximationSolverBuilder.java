6283372daaa8b707f8bced4ad94a73e58570aa4f
Thibault Falque <thibault.falque@uni.lu>
2025-03-13 10:35:23 +0100

    Refactor code & add Javadoc documentation

--- src/main/java/fr/univartois/cril/approximation/solver/ApproximationSolverBuilder.java
+++ src/main/java/fr/univartois/cril/approximation/solver/ApproximationSolverBuilder.java
@@ -23,28 +23,20 @@ package fr.univartois.cril.approximation.solver;
 import java.util.function.Supplier;
 
 import org.chocosolver.solver.Solver;
-import org.chocosolver.solver.search.restart.GeometricalCutoff;
-import org.chocosolver.solver.search.restart.LinearCutoff;
-import org.chocosolver.solver.search.restart.LubyCutoff;
 
 import fr.univartois.cril.approximation.core.IConstraintMeasure;
 import fr.univartois.cril.approximation.core.IConstraintsRemover;
 import fr.univartois.cril.approximation.core.KeepFalsifiedConstraintStrategy;
 import fr.univartois.cril.approximation.core.KeepNoGoodStrategy;
-import fr.univartois.cril.approximation.solver.sequence.ESequence;
-import fr.univartois.cril.approximation.solver.sequence.ExponentialSequence;
-import fr.univartois.cril.approximation.solver.sequence.ISequence;
-import fr.univartois.cril.approximation.solver.sequence.SequenceChocoCutOffDecorator;
 import fr.univartois.cril.approximation.solver.state.NormalStateSolver;
 import fr.univartois.cril.approximation.solver.state.SubApproximationStateSolver;
 import fr.univartois.cril.approximation.subapproximation.measure.ConstraintMeasureFactory;
 import fr.univartois.cril.approximation.subapproximation.measure.MeanFilteringConstraintMeasure;
 import fr.univartois.cril.approximation.subapproximation.remover.ConstraintRemoverFactory;
-import fr.univartois.cril.approximation.subapproximation.remover.PercentageConstraintRemover;
 import net.sourceforge.argparse4j.inf.Namespace;
 
 /**
- * The ApproximationSolverBuilder
+ * A builder for creating the approximation solver from the command line interface.
  *
  * @author Thibault Falque
  * @author Romain Wallon
@@ -53,145 +45,193 @@ import net.sourceforge.argparse4j.inf.Namespace;
  */
 public class ApproximationSolverBuilder {
 
-	private Solver solver;
+    /**
+     * The solver that will be decorated by the approximation solver.
+     */
+    private Solver solver;
 
-	private IApproximationSolver approxSolver;
+    /**
+     * The decorator that will be used to decorate the solver.
+     */
+    private ApproximationSolverDecorator decorator;
 
-	private ApproximationSolverDecorator decorator;
+    /**
+     * The supplier of the constraint remover that will be used to creates the remover of
+     * the constraints.
+     *
+     * @see IConstraintsRemover
+     */
+    private Supplier<IConstraintsRemover> remover;
 
-	private Supplier<IConstraintsRemover> remover;
+    /**
+     * The measure that will be used to select the constraint that we remove.
+     *
+     * @see fr.univartois.cril.approximation.core.IConstraintMeasure
+     */
+    private IConstraintMeasure measure;
 
-	private IConstraintMeasure measure;
+    /** The number of steps. */
+    private long nbSteps;
 
-	private double percentage;
+    /**
+     * Instantiates a new approximation solver builder.
+     *
+     * @param solver the solver
+     */
+    public ApproximationSolverBuilder(Solver solver) {
+        this.solver = solver;
+        decorator = new ApproximationSolverDecorator(solver.getModel());
+    }
 
-	private ISequence sequence;
-	
-	private long nbSteps;
+    /**
+     * Sets the keep nogood.
+     *
+     * @param value the value
+     *
+     * @return the approximation solver builder
+     */
+    public ApproximationSolverBuilder setKeepNogood(KeepNoGoodStrategy value) {
+        decorator.setKeepNogood(value);
+        return this;
+    }
 
-	public ApproximationSolverBuilder(Solver solver) {
-		this.solver = solver;
-		decorator = new ApproximationSolverDecorator(solver.getModel());
-		approxSolver = decorator;
-		sequence = new SequenceChocoCutOffDecorator(new LinearCutoff(Long.MAX_VALUE));
-	}
+    /**
+     * Sets the keep falsified.
+     *
+     * @param keep the keep
+     *
+     * @return the approximation solver builder
+     */
+    public ApproximationSolverBuilder setKeepFalsified(KeepFalsifiedConstraintStrategy keep) {
+        decorator.setKeepFalsified(keep);
+        return this;
+    }
 
-	public ApproximationSolverBuilder setKeepNogood(KeepNoGoodStrategy value) {
-		decorator.setKeepNogood(value);
-		return this;
-	}
+    /**
+     * Sets the verbosity of the decoree solver.
+     *
+     * @param v the level of verbosity
+     *
+     * @return the approximation solver builder with the verbosity set
+     */
+    public ApproximationSolverBuilder setVerbosity(int v) {
+        decorator.setVerbosity(v);
+        return this;
+    }
 
-	/**
-	 * @param object
-	 * @return
-	 */
-	public ApproximationSolverBuilder setKeepFalsified(KeepFalsifiedConstraintStrategy keep) {
-		decorator.setKeepFalsified(keep);
-		return this;
-	}
+    /**
+     * Sets the timeout using a string representation.
+     * For example, "10s" for 10 seconds or "100ms" for 100 milliseconds.
+     *
+     * @param timeout the timeout
+     *
+     * @return the approximation solver builder
+     */
+    public ApproximationSolverBuilder setTimeout(String timeout) {
+        if (timeout != null) {
+            if (timeout.contains("ms")) {
+                decorator.setTimeoutMs(Long.parseLong(timeout.replace("ms", "")));
+            } else if (timeout.contains("s")) {
+                decorator.setTimeout(Long.parseLong(timeout.replace("s", "")));
+            } else {
+                throw new IllegalArgumentException(
+                        timeout + " is not a correct format for set the timeout");
+            }
+        }
+        return this;
+    }
 
-	public ApproximationSolverBuilder setVerbosity(int v) {
-		decorator.setVerbosity(v);
-		return this;
-	}
+    /**
+     * Adds the kind of remover that will be used to remove the constraints.
+     *
+     * @param rm the name of the constraint remover
+     *
+     * @return the approximation solver builder
+     */
+    public ApproximationSolverBuilder withSpecificConstraintRemover(String rm) {
+        remover = () -> {
+            var r = ConstraintRemoverFactory.instance().createConstraintRemoverByName(rm,
+                    decorator);
+            r.setConstraintMeasure(measure);
+            return r;
+        };
+        return this;
+    }
 
-	public ApproximationSolverBuilder setTimeout(String timeout) {
-		if (timeout != null) {
-			if (timeout.contains("ms")) {
-				decorator.setTimeoutMs(Long.parseLong(timeout.replace("ms", "")));
-			} else if (timeout.contains("s")) {
-				decorator.setTimeout(Long.parseLong(timeout.replace("s", "")));
-			} else {
-				throw new IllegalArgumentException(timeout + " is not a correct format for set the timeout");
-			}
-		}
-		return this;
-	}
+    /**
+     * The maximum number of steps (a step is a normal state or a relaxation state) that
+     * the approximation solver will perform.
+     *
+     * @param nbSteps the number of steps
+     *
+     * @return the approximation solver builder with the number of steps added
+     */
+    public ApproximationSolverBuilder withNbStep(long nbSteps) {
+        this.nbSteps = nbSteps;
+        return this;
+    }
 
-	public ApproximationSolverBuilder withPercentage(double percentage) {
-		this.percentage = percentage;
-		return this;
-	}
+    /**
+     * Adds a measure that will be used to select the constraint that we remove.
+     *
+     * @param m the name of the measure
+     *
+     * @return the current builder with the measure added
+     */
+    public ApproximationSolverBuilder withSpecificConstraintMeasure(String m) {
+        measure = ConstraintMeasureFactory.instance().createConstraintMeasurerByName(m, decorator);
+        return this;
+    }
 
-	public ApproximationSolverBuilder withSpecificConstraintRemover(String rm) {
-		remover = () -> {
-			var r = ConstraintRemoverFactory.instance().createConstraintRemoverByName(rm, decorator);
-			r.setConstraintMeasure(measure);
-			if (r instanceof PercentageConstraintRemover) {
-				((PercentageConstraintRemover) r).setPercentage(percentage);
-			}
-			return r;
-		};
-		return this;
-	}
+    /**
+     * With mean computation.
+     *
+     * @param mean the mean
+     *
+     * @return the approximation solver builder
+     */
+    public ApproximationSolverBuilder withMeanComputation(boolean mean) {
+        if (mean) {
+            measure = new MeanFilteringConstraintMeasure(measure);
+            measure.setSolver(decorator);
+        }
+        return this;
+    }
 
-	public ApproximationSolverBuilder withSequenceApproximation(ESequence s, Namespace arguments) {
-		if (s == null) {
-			throw new IllegalArgumentException("Sequence cannot be null");
-		}
-		switch(s) {
-		case CHOCO_GEOMETRICAL:
-			sequence = new SequenceChocoCutOffDecorator(new GeometricalCutoff(arguments.getLong("lin_geo_factor"),arguments.getDouble("factor")));
-			break;
-		case CHOCO_LINEAR:
-			sequence = new SequenceChocoCutOffDecorator(new LinearCutoff(arguments.getLong("lin_geo_factor")));
-			break;
-		case CHOCO_LUBY:
-			sequence = new SequenceChocoCutOffDecorator(new LubyCutoff(arguments.getLong("lin_geo_factor")));
-			break;
-		case EXPONENTIAL:
-			sequence = new ExponentialSequence(arguments.getDouble("factor"));
-			break;
-		default:
-			break;
-		}
-		
-		return this;
-	}
-	
-	public ApproximationSolverBuilder withNbStep(long nbSteps) {
-		this.nbSteps = nbSteps;
-		return this;
-	}
+    /**
+     * Initializes the internal states of the relaxation solver.
+     *
+     * @param arguments the arguments
+     *
+     * @return the approximation solver builder
+     */
+    public ApproximationSolverBuilder initState(Namespace arguments) {
+        if (remover == null) {
+            throw new IllegalStateException("Constraint remover must be initialized !");
+        }
+        SubApproximationStateSolver.initInstance(solver, remover,
+                new SolverConfiguration(arguments.getInt("n_runs_approx"),
+                        arguments.getDouble("factor_runs_approx"),
+                        arguments.getInt("n_sol_limit"),
+                        arguments.getDouble("ratio_assigned_approx"),
+                        arguments.getBoolean("dichotomic_bound")),
+                arguments.get("path_strategy"));
+        NormalStateSolver.initInstance(solver,
+                new SolverConfiguration(arguments.getInt("n_runs_normal"),
+                        arguments.getDouble("factor_runs_normal"),
+                        Long.MAX_VALUE, arguments.getDouble("ratio_assigned_normal"),
+                        arguments.getBoolean("dichotomic_bound")),
+                decorator, arguments.get("path_strategy"));
+        return this;
+    }
 
-	public ApproximationSolverBuilder withSpecificConstraintMeasure(String m) {
-		measure = ConstraintMeasureFactory.instance().createConstraintMeasurerByName(m, decorator);
-		return this;
-	}
+    /**
+     * Builds the approximation solver.
+     *
+     * @return the approximation solver that has been built
+     */
+    public IApproximationSolver build() {
+        return decorator;
+    }
 
-	public ApproximationSolverBuilder withMeanComputation(boolean mean) {
-		if (mean) {
-			measure = new MeanFilteringConstraintMeasure(measure);
-			measure.setSolver(decorator);
-		}
-		return this;
-	}
-
-	public ApproximationSolverBuilder initState(Namespace arguments) {
-		if (remover == null) {
-			throw new IllegalStateException("Constraint remover must be initialized !");
-		}
-		SubApproximationStateSolver.initInstance(solver, remover,
-				new SolverConfiguration(arguments.getInt("n_runs_approx"), arguments.getDouble("factor_runs_approx"),
-						arguments.getInt("n_sol_limit"), arguments.getDouble("ratio_assigned_approx"),
-						arguments.getBoolean("dichotomic_bound")),
-				arguments.get("path_strategy"));
-		NormalStateSolver.initInstance(solver,
-				new SolverConfiguration(arguments.getInt("n_runs_normal"), arguments.getDouble("factor_runs_normal"),
-						Long.MAX_VALUE, arguments.getDouble("ratio_assigned_normal"),
-						arguments.getBoolean("dichotomic_bound")),
-				decorator, arguments.get("path_strategy"));
-		return this;
-	}
-
-	public ApproximationSolverBuilder setDichotomic(boolean dichotomic) {
-		if (dichotomic) {
-			approxSolver = new DichotomicOptimizationSolver(decorator, sequence, nbSteps);
-		}
-		return this;
-	}
-
-	public IApproximationSolver build() {
-		return approxSolver;
-	}
 }
